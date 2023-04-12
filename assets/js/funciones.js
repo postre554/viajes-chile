20d47e2d96be796f1083aff5509f83951feae7dd
postre554 <moglimon554@gmail.com>
2023-04-12 13:20:07 -0400

    primer commit_+comentario

---
+++ assets/js/funciones.js
@@ -0,0 +1,19 @@
+let boton = document.getElementById("sendIt");
+let mensaje = document.getElementById("mensaje_wild");
+let run = document.getElementById("cerrar_wild");
+let inicio = document.getElementById("inicio");
+
+boton.addEventListener("click", function() {
+  run.style.display = "block";
+  mensaje.style.display = "block";
+
+});
+
+run.addEventListener("click", function() {
+    mensaje.style.display = "none";
+    run.style.display = "none";
+  });
+
+  inicio.addEventListener("click", function() {
+    alert('Es momento de buscar un chuyo');
+  });
